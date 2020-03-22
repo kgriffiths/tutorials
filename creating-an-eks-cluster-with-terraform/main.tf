c302ea1e854d500ef8d3c81ab153f01a75b8e8bb
Kevin Griffiths <kgriffiths@users.noreply.github.com>
2020-03-22 01:02:30 -0400

    create eks cluster

--- creating-an-eks-cluster-with-terraform/main.tf
+++ creating-an-eks-cluster-with-terraform/main.tf
@@ -3,6 +3,26 @@ provider "aws" {
   region  = var.region
 }
 
+locals {
+  cluster_name = "tutorial-eks"
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks-cluster.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks-cluster.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "1.10"
+}
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -16,4 +36,20 @@ module "vpc" {
   tags = {
     Environment = "dev"
   }
+}
+
+module "eks-cluster" {
+  source = "terraform-aws-modules/eks/aws"
+  
+  cluster_name = local.cluster_name
+  vpc_id       = module.vpc.vpc_id
+  subnets      = module.vpc.private_subnets
+
+  worker_groups = [
+    {
+      name                          = "worker-group-1"
+      instance_type                 = "t2.small"
+      asg_desired_capacity          = 2
+    }
+  ]
 }
