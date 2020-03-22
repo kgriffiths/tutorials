81d647d582625582bc3f49f2b974644a66fc3a5f
Kevin Griffiths <kgriffiths@users.noreply.github.com>
2020-03-22 01:08:17 -0400

    add nat gateway and subnet tags to vpc

--- creating-an-eks-cluster-with-terraform/main.tf
+++ creating-an-eks-cluster-with-terraform/main.tf
@@ -33,6 +33,20 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
+  enable_nat_gateway  = true
+  single_nat_gateway  = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+
   tags = {
     Environment = "dev"
   }
